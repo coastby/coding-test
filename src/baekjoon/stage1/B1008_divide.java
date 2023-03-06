0319abc5fec76de6e455e541a64e830d64642286
JO YEJI <whdpwl2@gmail.com>
2023-03-06 10:40:23 +0900

    :sparkles: 백준 stage1 + README.md

--- src/baekjoon/stage1/B1008_divide.java
+++ src/baekjoon/stage1/B1008_divide.java
@@ -2,15 +2,14 @@ package baekjoon.stage1;
 
 import java.io.*;
 
-public class B1000_plus {
-
+public class B1008_divide {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		String[] nums = br.readLine().split(" ");
-		int a = Integer.parseInt(nums[0]);
-		int b = Integer.parseInt(nums[1]);
-		System.out.println(a + b);
+		double a = Double.parseDouble(nums[0]);
+		double b = Double.parseDouble(nums[1]);
+		System.out.println(a / b);
 
 	}
 }
