40399a86a9ea98cc7b1c02aae8ae7a19b4eaaa99
JO YEJI <whdpwl2@gmail.com>
2023-03-08 22:07:36 +0900

    :sparkles: 백준 단계별 stage5

---
+++ src/baekjoon/stage5/B2743_단어길이재기.java
@@ -0,0 +1,10 @@
+package baekjoon.stage5;
+
+import java.util.*;
+
+public class B2743_단어길이재기 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println(sc.nextLine().length());
+	}
+}
